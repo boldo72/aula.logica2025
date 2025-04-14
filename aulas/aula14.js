98bd7d31783fdbc703174528540bf288ba987f36
luizotavio <luiz.boldorini@aluno.senai.br>
2025-04-14 10:54:38 -0300

    aula 14 e exercicios

---
+++ aulas/aula14.js
@@ -0,0 +1,176 @@
+
+
+linha()
+console.log('SESI / SENAI');
+linha()
+
+function linha() {
+    console.log(' - - - - - - - - - - - - - - - - - - -');
+    
+}
+
+
+function cabecalho() {
+    linha()
+    console.log('          SESI / SENAI');
+    linha()
+     
+}
+cabecalho()
+
+
+
+function cabecalhoEscola (nomeEscola) {
+    linha()
+    console.log(nomeEscola);
+    linha()
+     
+}
+cabecalhoEscola('USP')
+
+function Soma (n1 , ) {
+    
+}
+
+function media(n1, n2) {
+    var resultado = (n1 + n2) / 2
+    return resultado
+}
+var valor = media(5, 8)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
